4be361dbe741c3bbec9cb12196ec7f1a64e0214b
dhazlettjr <d.hazlett.jr@gmail.com>
2018-01-26 11:18:28 -0600

    finished matching radio buttons with true and false

--- controllers/mushroomCtrl.js
+++ controllers/mushroomCtrl.js
@@ -1,14 +1,14 @@
 'use strict';
 
-angular.module("MushroomApp").controller("MushroomCtrl", function($scope, MushroomFactory) {
+angular.module("MushroomApp").controller("mushroomCtrl", function($scope, mushroomFactory) {
 
-  MushroomFactory.getMushrooms()
+  mushroomFactory.getMushrooms()
   .then( (mushroomData) => {
       $scope.mushroomArray = [];
-      for (let mushroom in mushroomData.data.mushrooms){
-          $scope.mushroomArray.push(mushroomData.data.mushrooms[mushroom]);
+      for (let mushroom in mushroomData.data){
+          $scope.mushroomArray.push(mushroomData.data[mushroom]);
       }
-    console.log($scope.mushroom);
+    console.log($scope.mushroomArray);
   });
 
 });
